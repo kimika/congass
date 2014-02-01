2cbe06d564571900a186bbeb6fbee7f42514c7ae
kimika <kimika8@gmail.com>
2014-01-31 23:35:11 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CongassApp::Application.config.secret_key_base = 'da56b8054b70b8d3a3beb62ab36cdfa0aea68c8471b2bc1a1858a78d9228a490d6ac782a0ab3c10004ec1b47575c12c0c77ea1848e9863503ed4be79b93369e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CongassApp::Application.config.session_store :cookie_store, key: '_congass_app_session'
